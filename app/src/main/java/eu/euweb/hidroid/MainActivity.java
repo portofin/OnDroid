7d4bc577a31eea675d95b6c1b4dec1bb01084f04
Niza <git@github.com:portofin/OnDroid.git>
2023-02-12 21:25:37 +0100

    Erste

---
+++ app/src/main/java/eu/euweb/hidroid/MainActivity.java
@@ -0,0 +1,16 @@
+package eu.euweb.hidroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
